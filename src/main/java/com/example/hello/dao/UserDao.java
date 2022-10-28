a9e4bc88dd3ba17b4dffdac96dca5d26b7257ec6
Universenn <dnwndhks98@gmail.com>
2022-10-28 15:48:17 +0900

    Spring UserDao, User 추가

--- src/main/java/com/example/hello/dao/UserDao.java
+++ src/main/java/com/example/hello/dao/UserDao.java
@@ -1,75 +1,53 @@
 package com.example.hello.dao;
 
-
 import com.example.hello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
 
-@Repository
 public class UserDao {
+    private DataSource dataSource;
 
-    private final DataSource dataSource;
-    private final JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
-    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+    public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
-        this.jdbcTemplate = jdbcTemplate;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-
-    public void add(User user){
-        this.jdbcTemplate.update("insert into  users(id, name, password) values (?,?,?)",
-                user.getId(),user.getName(),user.getPassword());
-    }
+    private Connection c;
+    private PreparedStatement ps;
 
     public void deleteAll(){
 //        this.dataSource = dataSource;
         this.jdbcTemplate.update("delete from users");
     }
-    public User findById(String id){
-        Map<String, String> env = System.getenv();
-        Connection c;
-        PreparedStatement ps;
-        ResultSet rs;
 
-        try {
-            // DB 접속
-            c = dataSource.getConnection();
-
-            // Query 작성
-            ps = c.prepareStatement("select * from users where id = ?");
-            ps.setString(1,id);
-
-            // Query 실행
-            rs = ps.executeQuery();
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
-            }
-
-            rs.close();
-            ps.close();
-            c.close();
-
-            if (user == null) throw new RuntimeException();
-
-            return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void add(User user){
+        this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)",user.getId(),user.getName(),user.getPassword());
 
     }
+
+    public User findById(String id){
+        String sql = "SELECT * FROM users WHERE id = ?";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+                return user;
+            }
+        };
+        return jdbcTemplate.queryForObject(sql,rowMapper,id);
+    }
+
+
     public int getCount(){
         String sql = "SELECT count(*) FROM users";
         return jdbcTemplate.queryForObject(sql,Integer.class);
     }
-
 }
