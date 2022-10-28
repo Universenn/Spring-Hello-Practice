d466d7cfc0e7e18bd660bd06761f18b210ddb4b1
Universenn <dnwndhks98@gmail.com>
2022-10-28 15:56:15 +0900

    Spring deleteById 메서드 생성

--- src/main/java/com/example/hello/dao/UserDao.java
+++ src/main/java/com/example/hello/dao/UserDao.java
@@ -11,37 +11,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
-
-    private Connection c;
-    private PreparedStatement ps;
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+            return user;
+        }
+    };
 
     public void deleteAll(){
-//        this.dataSource = dataSource;
         this.jdbcTemplate.update("delete from users");
     }
 
     public void add(User user){
         this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)",user.getId(),user.getName(),user.getPassword());
-
     }
 
     public User findById(String id){
         String sql = "SELECT * FROM users WHERE id = ?";
-        RowMapper<User> rowMapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
-                return user;
-            }
-        };
         return jdbcTemplate.queryForObject(sql,rowMapper,id);
     }
 
@@ -49,5 +44,10 @@ public class UserDao {
     public int getCount(){
         String sql = "SELECT count(*) FROM users";
         return jdbcTemplate.queryForObject(sql,Integer.class);
+    }
+
+    public void deleteById(String id){
+        String sql = "delete from users where id = ?";
+        this.jdbcTemplate.queryForObject(sql,rowMapper,id);
     }
 }
